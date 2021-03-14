4b4b12a45c7ba1d47de8002310efa7c2487e90be
Adeniyi <35885369+apiprincz@users.noreply.github.com>
2021-03-14 13:54:51 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    BASE_URL: "http://localhost:3000",
-
-    MONGODB_URL:
-      "mongodb+srv://pave:Aoboa318%24@cluster0.daeub.mongodb.net/PAVE?retryWrites=true&w=majority",
-  },
-};
