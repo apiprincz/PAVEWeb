1b6970a6f2319f44d6442f0f3c78ac7c51323484
George660 <76912644+George660@users.noreply.github.com>
2021-03-26 07:12:15 +0100

    tweak the folder casing

--- pages/api/winner/[id].js
+++ pages/api/winner/[id].js
@@ -1,7 +1,7 @@
 import dbConnect from "../../../util/dbConnect";
 import dotenv from "dotenv";
 
-import Products from "../../../Models/productModel";
+import Products from "../../../models/productModel";
 import Cors from "cors";
 
 dotenv.config({ path: "ENV_FILENAME" });
