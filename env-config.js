4a4454bf76a50d3f48f78798f8950b0848426478
George660 <76912644+George660@users.noreply.github.com>
2021-03-27 20:01:27 +0100

    update babel

--- env-config.js
+++ env-config.js
@@ -1,6 +1,6 @@
 const prod = process.env.NODE_ENV === "production";
 module.exports = {
   "process.env.NEXT_PUBLIC_BASE_URL": prod
-    ? "pave.vercel.app"
+    ? "http://pave.vercel.app"
     : "http://localhost:3000",
 };
